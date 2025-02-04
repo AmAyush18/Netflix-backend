47a15ae6fb02af8d54952be8f0b2d0574c56ad68
Ayush Dwivedi <amayush0818@gmail.com>
2025-02-04 14:52:30 +0530

    Delete and Get all from mongodb completed

--- controller/controller.go
+++ controller/controller.go
@@ -6,10 +6,10 @@ import (
 	"log"
 	"netflix-backend/model"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 const connectionString = "mongodb+srv://amayush0818:gobackend123@golang-backend.zachn.mongodb.net/?retryWrites=true&w=majority&appName=golang-backend"
@@ -42,6 +42,7 @@ func init() {
 	fmt.Println("Collection instance is ready")
 }
 
+// insert one movie
 func insertOneMovie(movie model.Netflix) {
 	inserted, err := collection.InsertOne(context.Background(), movie)
 
@@ -52,6 +53,7 @@ func insertOneMovie(movie model.Netflix) {
 	fmt.Println("Inserted 1 movie in db with id: ", inserted.InsertedID)
 }
 
+// update one movie
 func updateOneMovie(movieId string) {
 	id, err := primitive.ObjectIDFromHex(movieId)
 
@@ -69,4 +71,56 @@ func updateOneMovie(movieId string) {
 	}
 
 	fmt.Println("modified count: ", result.ModifiedCount)
+}
+
+// delete one record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
+}
+
+// delete all records from mongodb
+func deleteAllMovie() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of movies delete: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+}
+
+// get all movies from database
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var movies []primitive.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		movies = append(movies, movie)
+	}
+
+	defer cursor.Close(context.Background())
+
+	return movies
 }
