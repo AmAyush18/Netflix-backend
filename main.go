6a3a61eb74798288e7d6b1d5a4421e14125a478d
Ayush Dwivedi <amayush0818@gmail.com>
2025-02-06 23:50:07 +0530

    Added Routes

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"netflix-backend/router"
+)
 
 func main() {
 	fmt.Println("Netflix Backend Watchlist")
+	r := router.Router()
+
+	fmt.Println("Server is getting started")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000 ...")
 }
